c190ce9e85ecf687286ffcfb7715289681aab5cb
muhammadirgi <143006217+MirgiNB@users.noreply.github.com>
2023-09-11 20:18:38 +0700

    Add files via upload

---
+++ JavaSriptDasar/index.js
@@ -0,0 +1,21 @@
+console.log('javascript 7'); console.log('hello world');
+
+var nama = 'Irgi';
+console.log(nama);
+
+nama = "nurul";
+console.log(nama);
+
+let alamat = 'Malang';
+console.log(alamat);
+
+let bilangan =1;
+console.log(bilangan); //1
+
+console.log(100/0); //infinity
+
+console.log("JavaScript / 2") //NaN
+
+console.log(typeof "JavaScript");
+
+
